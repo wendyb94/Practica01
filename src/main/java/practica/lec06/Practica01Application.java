8213ba2563a4e17534f418263c392ae3a13a1862
webadill <webadill@LAPTOP-8FSIJF7T>
2025-10-23 09:46:03 -0600

    ParteWendy

---
+++ src/main/java/practica/lec06/Practica01Application.java
@@ -0,0 +1,13 @@
+package practica.lec06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica01Application.class, args);
+	}
+
+}
